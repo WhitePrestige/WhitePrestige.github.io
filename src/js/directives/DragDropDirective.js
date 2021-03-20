5bbd563f7ea7bdcc01cfaaf37d213a13610b8f22
Михаил Яньшин <mikhail.yanshin@x5.ru>
2021-03-20 16:26:36 +0300

    Добавил drag n drop директиву

--- src/js/directives/DragDropDirective.js
+++ src/js/directives/DragDropDirective.js
@@ -2,6 +2,34 @@ import Vue from 'vue';
 
 Vue.directive('drag-drop', {
   bind: (el) => {
-    console.log(el)
+    el.onmousedown = function(e) { // 1. отследить нажатие
+
+      // подготовить к перемещению
+      // 2. разместить на том же месте, но в абсолютных координатах
+      el.style.position = 'absolute';
+      moveAt(e);
+      // переместим в body, чтобы мяч был точно не внутри position:relative
+      document.body.appendChild(el);
+
+      el.style.zIndex = 1000; // показывать мяч над другими элементами
+
+      // передвинуть мяч под координаты курсора
+      // и сдвинуть на половину ширины/высоты для центрирования
+      function moveAt(e) {
+        el.style.left = e.pageX - el.offsetWidth / 2 + 'px';
+        el.style.top = e.pageY - el.offsetHeight / 2 + 'px';
+      }
+
+      // 3, перемещать по экрану
+      document.onmousemove = function(e) {
+        moveAt(e);
+      }
+
+      // 4. отследить окончание переноса
+      el.onmouseup = function() {
+        document.onmousemove = null;
+        el.onmouseup = null;
+      }
+    }
   },
 });
