5db72acea0b7e2770f0d2f1b4714561694044123
Михаил Яньшин <mikhail.yanshin@x5.ru>
2021-03-20 17:05:54 +0300

    Кастомизировал dragNDrop

--- src/pages/DragDrop.vue
+++ src/pages/DragDrop.vue
@@ -1,20 +1,51 @@
 <template>
   <div class="drag-drop-page">
-    <div id='cube' v-drag-drop class="draggable"></div>
+    <div v-for="item in cubes">
+      <div v-drag-drop class="draggable" :style="{ backgroundColor: item.color }">
+        {{ item.id }}
+      </div>
+    </div>
+    <button @click="addCube">Добавить кубик</button>
   </div>
 </template>
 
 <script>
+import {colors} from '@/js/constants/colors.js'
+
 export default {
   name: 'DragDrop',
+  methods: {
+    addCube() {
+      this.cubes.push({id: this.id, color: colors[this.id]})
+      this.id++
+    }
+  },
+  data() {
+    return {
+      id: 0,
+      cubes: [],
+    }
+  },
 }
 </script>
 
 <style>
+.drag-drop-page {
+  outline: none;
+}
+
 .draggable {
   height: 50px;
   width: 50px;
-  background-color: darkolivegreen;
+  overflow: hidden;
+  cursor: pointer;
+}
+
+button {
+  position: absolute;
+  bottom: 0;
+  width: 180px;
+  height: 50px;
 }
 
 </style>
